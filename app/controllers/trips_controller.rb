810d8ca0dd796e5cea19c857d7684c3e7a354504
Mickael Couderc <mklcouderc@gmail.com>
2022-06-01 16:06:31 +0200

    index trip created

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,26 @@
+class TripsController < ApplicationController
+
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
