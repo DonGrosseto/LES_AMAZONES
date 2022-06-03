770171219af6be317372cc18fb1ddd78a2d6a35c
benjamin grosset <benjamin.grosset2@gmail.com>
2022-06-03 15:52:26 +0200

    récupération des coordonnées lat et long sur modèle trip - en attente implantation de mapbox pour affichage de la carte

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,18 @@ class Trip < ApplicationRecord
   belongs_to :chatroom
 
   validates :transport, inclusion: { in: ["piéton", "vélo", "voiture", "taxi", "métro"] }, presence: true
+
+  before_save :geocode_endpoints
+
+  private
+  #To enable Geocoder to works with multiple locations
+  def geocode_endpoints
+    coord_start=Geocoder.search(self.starting_point).first.coordinates
+        self.latitude_start = coord_start[0]
+        self.longitude_start = coord_start[1]
+
+    coord_end=Geocoder.search(self.ending_point).first.coordinates
+        self.latitude_end = coord_end[0]
+        self.longitude_end = coord_end[1]
+  end
 end
