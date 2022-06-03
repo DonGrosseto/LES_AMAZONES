51f025ef2aa827fb419a6c68cefd14d66420b7b4
Mickael Couderc <mklcouderc@gmail.com>
2022-06-03 15:29:17 +0200

    add authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone_number])
