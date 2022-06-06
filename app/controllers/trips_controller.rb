c21fc8a7aabc58a3291f195487d75eaa4f9ff5b9
benjamin grosset <benjamin.grosset2@gmail.com>
2022-06-06 13:16:26 +0200

    mapbox+view map - to do draw between points

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,6 @@
 class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update]
   def index
-
     if params[:starting_point].present? && params[:ending_point].present?
       @trips = Trip.where(starting_point: params[:starting_point], ending_point: params[:ending_point],date: params[:date])
     else
@@ -10,6 +9,14 @@ class TripsController < ApplicationController
   end
 
   def show
+    @points = {start:[@trip.latitude_start, @trip.longitude_start], end:[@trip.latitude_end, @trip.longitude_end]}
+    @markers = @points.map do |point|
+      point.flatten!
+      {
+        lat: point[1],
+        lng: point[2]
+      }
+    end
   end
 
   def new
@@ -20,9 +27,8 @@ class TripsController < ApplicationController
     @trip = Trip.new(trip_params)
     @trip.user = current_user
     @trip.chatroom = Chatroom.create
-    rating = Rating.create
     if @trip.save
-      Booking.create(trip_id: @trip.id, user_id: current_user.id, rating_id: rating.id)
+      @booking = Booking.create(trip_id: @trip, user_id: current_user, rating_id: Rating.new)
       redirect_to trip_path(@trip)
     else
       render :new
