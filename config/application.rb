ca50390e54d0d1a4f1721f2cd318040df0f1c970
Germe Marc Antoine <markus0059@hotmail.fr>
2022-06-07 13:22:39 +0200

    return err

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module LESAmazones
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     config.generators do |generate|
       generate.assets false
       generate.helper false
