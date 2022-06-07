3ca1e597923bfe3a03fc1de87d13568dbf105e7e
Mickael Couderc <mklcouderc@gmail.com>
2022-06-07 13:20:08 +0200

    precompile fixed

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module LESAmazones
   class Application < Rails::Application
@@ -15,6 +16,8 @@ module LESAmazones
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
