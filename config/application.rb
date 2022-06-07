e990017cdf374dc21677b790e50f7b7fe7737fea
Mickael Couderc <mklcouderc@gmail.com>
2022-06-07 13:17:52 +0200

    precompile fixed

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module LESAmazones
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
