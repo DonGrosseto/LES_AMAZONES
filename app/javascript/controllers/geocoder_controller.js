f700f7c90696b821eef12d20dfa0838b86fa2d6a
benjamin grosset <benjamin.grosset2@gmail.com>
2022-06-08 11:14:19 +0200

    it works

--- app/javascript/controllers/geocoder_controller.js
+++ app/javascript/controllers/geocoder_controller.js
@@ -4,7 +4,7 @@ import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 export default class extends Controller {
   static values = { apiKey: String }
 
-  static targets = ["starting_point", "ending_point"]
+  static targets = ["address"]
 
   connect() {
     this.geocoder = new MapboxGeocoder({
@@ -12,5 +12,17 @@ export default class extends Controller {
       types: "country,region,place,postcode,locality,neighborhood,address"
     });
     this.geocoder.addTo(this.element)
+    console.log("1", this.startingTarget)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
   }
+
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
+  }
+
 }
