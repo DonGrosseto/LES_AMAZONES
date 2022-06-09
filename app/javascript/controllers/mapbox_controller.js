dd2f1d12f585965fdc17ae096d9b06bfd744b168
Germe Marc Antoine <markus0059@hotmail.fr>
2022-06-09 18:09:58 +0200

    fix map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -20,9 +20,12 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-    this.#addRoute()
+    this.map.on("load", () => {
+
+      this.#addMarkersToMap()
+      this.#fitMapToMarkers()
+      this.#addRoute()
+    })
 
   }
   #addMarkersToMap() {
@@ -30,64 +33,64 @@ export default class extends Controller {
       const el = document.createElement('div');
       el.className = 'marker';
       new mapboxgl.Marker(el)
-        .setLngLat([ trip.lng, trip.lat ])
+        .setLngLat([trip.lng, trip.lat])
         .addTo(this.map)
+    });
+  }
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    this.map.fitBounds(bounds, { padding: 30, maxZoom: 10, duration: 0 })
+  }
+
+  #drawRoute(coord) {
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: coord
+      }
+    };
+    if (this.map.getSource('route')) {
+      this.map.getSource('route').setData(geojson);
+    }
+    else {
+      this.map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#D06224',
+          'line-width': 4,
+          'line-opacity': 1
+        }
       });
     }
-    #fitMapToMarkers() {
-      const bounds = new mapboxgl.LngLatBounds()
-      this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-      this.map.fitBounds(bounds, { padding: 30, maxZoom: 10, duration: 0 })
-    }
 
-    #drawRoute(coord){
-      const geojson = {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'LineString',
-              coordinates: coord
-            }
-          };
-          if (this.map.getSource('route')) {
-            this.map.getSource('route').setData(geojson);
-          }
-          else {
-            this.map.addLayer({
-              id: 'route',
-              type: 'line',
-              source: {
-                type: 'geojson',
-                data: geojson
-              },
-              layout: {
-                'line-join': 'round',
-                'line-cap': 'round'
-              },
-              paint: {
-                'line-color': '#D06224',
-                'line-width': 4,
-                'line-opacity': 1
-              }
-            });
-          }
-
-    }
-
-    #addRoute() {
-      if (this.steps.length > 21) {
-        // async function getRoute() {
-          fetch(
-            `https://api.mapbox.com/directions/v5/mapbox/driving/${this.steps.substring(0, this.steps.length - 1)}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`)
-            .then(response => response.json())
-            .then(data =>  {
-              const coord = data.routes[0].geometry.coordinates;
-              console.log(coord);
-              this.#drawRoute(coord);
-            });
-
-          //
-      }
-
-    }
   }
+
+  #addRoute() {
+    if (this.steps.length > 21) {
+      // async function getRoute() {
+      fetch(
+        `https://api.mapbox.com/directions/v5/mapbox/driving/${this.steps.substring(0, this.steps.length - 1)}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`)
+        .then(response => response.json())
+        .then(data => {
+          const coord = data.routes[0].geometry.coordinates;
+          console.log(coord);
+          this.#drawRoute(coord);
+        });
+
+      //
+    }
+
+  }
+}
